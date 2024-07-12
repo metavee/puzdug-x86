2642526a88f8afa4f9c062dd3a55d3119c463929
Robin Neufeld <metavee@users.noreply.github.com>
2024-07-12 17:00:44 -0400

    chatgpt newline

--- puzdug.asm
+++ puzdug.asm
@@ -1,7 +1,7 @@
     org 0x0100
 
 level: equ 0x0300
-board_size: equ 3
+board_size: equ 400
 
 wipe_level:
     mov bx,level
@@ -15,19 +15,24 @@ wipe_loop:
 show_board:
     mov bx,level
     mov cx,board_size
+    mov dl,0
 show_loop:
     mov al,[bx]
     call display_letter
 
-    ; ; move to new line if (bx+1) % 20 == 0
-    ; push ax
-    ; push cx
-    ; push dx
+    inc dl
+    ; if dl >= 20 then reset to 0 and print newline
+    cmp dl, 20
+    jl continue_loop
+    mov ah, 0x02  ; Function to move cursor to start (carriage return)
+    mov dl, 0x0D  ; Carriage return character
+    int 0x21      ; DOS interrupt to perform output
+    mov ah, 0x02  ; Function to output
+    mov dl, 0x0A  ; Line feed character
+    int 0x21      ; DOS interrupt to perform output
+    mov dl, 0
 
-    ; pop dx
-    ; pop cx
-    ; pop ax
-
+continue_loop:
     inc bx
     loop show_loop
 
