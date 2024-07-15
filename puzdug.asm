5f2f57ed9b72e94a36c05af88ea7427cc68c4863
Robin Neufeld <metavee@users.noreply.github.com>
2024-07-15 15:04:34 -0400

    fix segments and have player xy

--- puzdug.asm
+++ puzdug.asm
@@ -1,6 +1,8 @@
 org 0x0100
 
-player_pos: equ 0x0200      ; 2 bytes for player position
+; TODO: should I just use 2 bytes for both of x and y? easy to get confused with single bytes
+player_x: equ 0x0200      ; 2 bytes for player position
+player_y: equ 0x0201
 
 level_dim: equ 25
 level_size: equ 400         ; level_dim^2
@@ -13,15 +15,21 @@ row_width: equ 80           ; Width of the screen in characters
 wall_char: equ 0x04b2 ; black bg, red fg, heavy texture ▓
 empty_char: equ 0x07b0 ; black bg, grey fg, light texture ░
 fog_char: equ 0x07f7 ; black bg, grey fg, almost equal ≈
+player_char: equ 0x0f40 ; black bg, white fg, @
 
 start:
     mov ax,0x0002 ; set video mode to color text
     int 0x10 ; call interrupt to set video mode
 
     ; Set up segments
-    mov ax, video_segment
+    mov ax,cs
     mov ds,ax
+    mov ax, video_segment
     mov es,ax
+
+    ; Set player coordinate
+    mov byte [player_x],5
+    mov byte [player_y],7
 
 init_board:
     cld ; clear direction flag - di will increment
@@ -30,8 +38,8 @@ init_board:
     mov cx,level_size
     xor dx,dx
 init_board_loop:
-    ; store empty_char everywhere on screen
-    mov ax, empty_char
+    ; store everywhere on screen
+    mov ax, fog_char
     stosw
 
     ; newline logic
@@ -43,6 +51,18 @@ init_board_loop:
 
 continue_board_loop:
     loop init_board_loop          ; Decrement CX, if CX != 0, loop back
+
+draw_player:
+    movzx ax, byte [player_y] ; Zero-extend player_y to ax
+    mov dl,row_width*2
+    mul dl
+    movzx dx, byte [player_x] ; Zero-extend player_x to dx
+    add ax, dx
+    add ax, dx
+    mov di,ax
+    ; mov di,[player_pos]
+    mov ax, player_char
+    stosw
 
     ; press any key to exit
     call read_keyboard
