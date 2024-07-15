a6effecd6a93df9a6198ae78debc22c1655d71e2
Robin Neufeld <metavee@users.noreply.github.com>
2024-07-15 11:33:57 -0400

    display player (this is not scalable)

--- puzdug.asm
+++ puzdug.asm
@@ -1,7 +1,10 @@
     org 0x0100
 
+player_pos: equ 0x0200 ; 2 byte
 level: equ 0x0300
 board_size: equ 400
+
+    mov word [player_pos], 0 ; starting position
 
 wipe_level:
     mov bx,level
@@ -17,9 +20,22 @@ show_board:
     mov cx,board_size
     mov dl,0
 show_loop:
+    ; check if we are at player_pos and print
+    push bx
+    mov bx,400
+    sub bx,[player_pos]
+    cmp cx, bx
+    pop bx
+    jne board_letter
+    mov al,'@'
+    call display_letter
+    jmp board_newline
+
+board_letter:
     mov al,[bx]
     call display_letter
 
+board_newline:
     inc dl
     ; if dl >= 20 then reset to 0 and print newline
     cmp dl, 20
